42632bd4527881928ac4031deafc74bbe22b01b2
Richard Heard <richie@rheard.com>
2014-06-21 14:51:07 -0700

    Static format string...

--- MASShortcutView.m
+++ MASShortcutView.m
@@ -379,9 +379,9 @@ void *kUserDataHint = &kUserDataHint;
                             [weakSelf activateEventMonitoring:NO];
                             NSString *format = NSLocalizedString(@"The key combination %@ cannot be used",
                                                                  @"Title for alert when shortcut is already used");
-                            NSRunCriticalAlertPanel([NSString stringWithFormat:format, shortcut], error.localizedDescription,
+                            NSRunCriticalAlertPanel(format, error.localizedDescription,
                                                     NSLocalizedString(@"OK", @"Alert button when shortcut is already used"),
-                                                    nil, nil);
+                                                    nil, nil, shortcut);
                             weakSelf.shortcutPlaceholder = nil;
                             [weakSelf activateResignObserver:YES];
                             [weakSelf activateEventMonitoring:YES];

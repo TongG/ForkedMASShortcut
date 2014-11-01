1dfaccc2d138635bff4328582b989caea0ea83fa
Tong Guo <Tong-G@outlook.com>
2014-11-01 11:17:32 +0800

    Some format.

--- MASShortcut.h
+++ MASShortcut.h
@@ -1,63 +1,71 @@
 #import <Carbon/Carbon.h>
 
-#define MASShortcutChar(char) [NSString stringWithFormat:@"%C", (unsigned short)(char)]
-#define MASShortcutClear(flags) (flags & (NSControlKeyMask | NSShiftKeyMask | NSAlternateKeyMask | NSCommandKeyMask))
-#define MASShortcutCarbonFlags(cocoaFlags) (\
-    (cocoaFlags & NSCommandKeyMask ? cmdKey : 0) | \
-    (cocoaFlags & NSAlternateKeyMask ? optionKey : 0) | \
-    (cocoaFlags & NSControlKeyMask ? controlKey : 0) | \
-    (cocoaFlags & NSShiftKeyMask ? shiftKey : 0))
+/* Convert the C-style char to NSString object */
+#define MASShortcutChar( _Char ) [ NSString stringWithFormat: @"%C", ( unsigned short )( _Char ) ]
 
-// These glyphs are missed in Carbon.h
-enum {
-    kMASShortcutGlyphEject = 0x23CF,
-    kMASShortcutGlyphClear = 0x2715,
-	kMASShortcutGlyphDeleteLeft = 0x232B,
-	kMASShortcutGlyphDeleteRight = 0x2326,
-    kMASShortcutGlyphLeftArrow = 0x2190,
-	kMASShortcutGlyphRightArrow = 0x2192,
-	kMASShortcutGlyphUpArrow = 0x2191,
-	kMASShortcutGlyphDownArrow = 0x2193,
-	kMASShortcutGlyphEscape = 0x238B,
-	kMASShortcutGlyphHelp = 0x003F,
-    kMASShortcutGlyphPageDown = 0x21DF,
-	kMASShortcutGlyphPageUp = 0x21DE,
-	kMASShortcutGlyphTabRight = 0x21E5,
-	kMASShortcutGlyphReturn = 0x2305,
-	kMASShortcutGlyphReturnR2L = 0x21A9,	
-	kMASShortcutGlyphPadClear = 0x2327,
-	kMASShortcutGlyphNorthwestArrow = 0x2196,
-	kMASShortcutGlyphSoutheastArrow = 0x2198,
-} MASShortcutGlyph;
+/* Leaving only the NSControlKeyMask, NSShiftKeyMask, NSAlternateKeyMask and NSCommandKeyMask
+ * filter out all of others */
+#define MASShortcutClear( _Flags ) ( _Flags & ( NSControlKeyMask | NSShiftKeyMask | NSAlternateKeyMask | NSCommandKeyMask ) )
 
-@interface MASShortcut : NSObject <NSCoding> {
-    NSUInteger _keyCode; // NSNotFound if empty
-    NSUInteger _modifierFlags; // 0 if empty
-}
+#define MASShortcutCarbonFlags( _CocoaFlags ) (                      \
+    ( _CocoaFlags & NSCommandKeyMask ? cmdKey : 0 ) |                \
+    ( _CocoaFlags & NSAlternateKeyMask ? optionKey : 0 ) |           \
+    ( _CocoaFlags & NSControlKeyMask ? controlKey : 0 ) |            \
+    ( _CocoaFlags & NSShiftKeyMask ? shiftKey : 0 ) )
 
-@property (nonatomic) NSUInteger keyCode;
-@property (nonatomic) NSUInteger modifierFlags;
-@property (nonatomic, readonly) UInt32 carbonKeyCode;
-@property (nonatomic, readonly) UInt32 carbonFlags;
-@property (nonatomic, readonly) NSString *keyCodeString;
-@property (nonatomic, readonly) NSString *keyCodeStringForKeyEquivalent;
-@property (nonatomic, readonly) NSString *modifierFlagsString;
-@property (nonatomic, readonly) NSData *data;
-@property (nonatomic, readonly) BOOL shouldBypass;
-@property (nonatomic, readonly, getter = isValid) BOOL valid;
+/* These glyphs are missed in Carbon.h */
+enum { kMASShortcutGlyphEject = 0x23CF
+     , kMASShortcutGlyphClear = 0x2715
+     , kMASShortcutGlyphDeleteLeft = 0x232B
+     , kMASShortcutGlyphDeleteRight = 0x2326
+     , kMASShortcutGlyphLeftArrow = 0x2190
+	 , kMASShortcutGlyphRightArrow = 0x2192
+	 , kMASShortcutGlyphUpArrow = 0x2191
+	 , kMASShortcutGlyphDownArrow = 0x2193
+	 , kMASShortcutGlyphEscape = 0x238B
+	 , kMASShortcutGlyphHelp = 0x003F
+     , kMASShortcutGlyphPageDown = 0x21DF
+	 , kMASShortcutGlyphPageUp = 0x21DE
+	 , kMASShortcutGlyphTabRight = 0x21E5
+	 , kMASShortcutGlyphReturn = 0x2305
+	 , kMASShortcutGlyphReturnR2L = 0x21A9
+	 , kMASShortcutGlyphPadClear = 0x2327
+	 , kMASShortcutGlyphNorthwestArrow = 0x2196
+	 , kMASShortcutGlyphSoutheastArrow = 0x2198
+     } MASShortcutGlyph;
 
-- (id)initWithKeyCode:(NSUInteger)code modifierFlags:(NSUInteger)flags;
+@interface MASShortcut : NSObject <NSCoding>
+    {
+    NSUInteger _keyCode;        // NSNotFound if empty
+    NSUInteger _modifierFlags;  // 0 if empty
+    }
 
-+ (MASShortcut *)shortcutWithKeyCode:(NSUInteger)code modifierFlags:(NSUInteger)flags;
-+ (MASShortcut *)shortcutWithEvent:(NSEvent *)anEvent;
-+ (MASShortcut *)shortcutWithData:(NSData *)aData;
+@property ( nonatomic ) NSUInteger keyCode;
+@property ( nonatomic ) NSUInteger modifierFlags;
+@property ( nonatomic, readonly ) UInt32 carbonKeyCode;
+@property ( nonatomic, readonly ) UInt32 carbonFlags;
+@property ( nonatomic, readonly ) NSString* keyCodeString;
+@property ( nonatomic, readonly ) NSString* keyCodeStringForKeyEquivalent;
+@property ( nonatomic, readonly ) NSString* modifierFlagsString;
+@property ( nonatomic, readonly ) NSData* data;
+@property ( nonatomic, readonly ) BOOL shouldBypass;
+@property ( nonatomic, readonly, getter = isValid ) BOOL valid;
 
-- (BOOL)isTakenError:(NSError **)error;
+- ( id ) initWithKeyCode: ( NSUInteger )_Code
+           modifierFlags: ( NSUInteger )_Flags;
 
-// The following API enable hotkeys with the Option key as the only modifier
-// For example, Option-G will not generate © and Option-R will not paste ®
-+ (void)setAllowsAnyHotkeyWithOptionModifier:(BOOL)allow;
-+ (BOOL)allowsAnyHotkeyWithOptionModifier;
++ ( MASShortcut* ) shortcutWithKeyCode: ( NSUInteger )_Code
+                         modifierFlags: ( NSUInteger )_Flags;
+
++ ( MASShortcut* ) shortcutWithEvent: ( NSEvent* )_AnEvent;
++ ( MASShortcut* ) shortcutWithData: ( NSData* )_Data;
+
+- ( BOOL ) isTakenError: ( NSError** )_Error;
+
+// The following API enable hotkeys with the ⌥ key as the only modifier
+// For example, ⌥G will not generate © and ⌥R will not paste ®
++ ( void ) setAllowsAnyHotkeyWithOptionModifier: ( BOOL )_Allow;
++ ( BOOL ) allowsAnyHotkeyWithOptionModifier;
 
 @end
 
